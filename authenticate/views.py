c611d6a9cd96d4a11bb11f1fc6a286395fa9a5a7
Shubham Kumar <66840922+Shubham5213@users.noreply.github.com>
2024-01-03 22:36:47 +0530

    Built first django Project

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
-from .forms import SignUpForm
+from authenticate.forms import SignUpForm
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 
 def home(request): 
 	return render(request, 'home.html')
 
-def login_user (request):
-	if request.method == 'POST': #if someone fills out form , Post it 
+def login_user(request):
+	if request.method == 'POST': #if someone fills out form , Post it
 		username = request.POST['username']
 		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
+		try:
+			user = authenticate(request, username=User.objects.get(email= username), password=password)
+		except:
+			user = authenticate(request, username=username, password=password)
 		if user is not None:# if user exist
 			login(request, user)
-			messages.success(request,('Your logged in'))
+			messages.success(request,('Login Successful!!'))
 			return redirect('home') #routes to 'home' on successful login  
 		else:
-			messages.success(request,('Error logging in'))
+			messages.success(request,('Email or Password is Incorrect!!'))
 			return redirect('login') #re routes to login page upon unsucessful login
 	else:
 		return render(request, 'login.html', {})
 
 def logout_user(request):
 	logout(request)
-	messages.success(request,('Youre now logged out'))
+	messages.success(request,('Logged Out Successfully!'))
 	return redirect('home')
 
 def register_user(request):
@@ -37,7 +42,7 @@ def register_user(request):
 			password = form.cleaned_data['password1']
 			user = authenticate(username=username, password=password)
 			login(request,user)
-			messages.success(request, ('Youre now registered'))
+			messages.success(request, ('Successfully Registered!'))
 			return redirect('home')
 	else: 
 		form = SignUpForm() 
